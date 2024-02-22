39246ce60decf7dd563f4d244db1aa1042ceaa23
GerasH-Solicy <geras.harutyunyan@solicy.net>
2024-02-22 17:54:57 +0400

    Updated db url

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,9 @@ app.use(cors());
 
 const port = 8000;
 
-const mongoURI = "mongodb://localhost:27017/apis";
+const mongoURI =
+  "mongodb+srv://root:geras2004.@cluster0.p7ntirq.mongodb.net/?retryWrites=true&w=majority";
+
 
 mongoose.connect(mongoURI);
 
